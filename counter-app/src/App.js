dc156b82296ffec1b5174be461899e7e915cf498
Adawat <adawat.c@kbtg.tech>
2020-04-20 00:48:16 +0700

    Do not show total counters (Still need to fixed)

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/navbar";
 import Counters from "./components/counters";
 
-function App() {
-  return (
-    <React.Fragment>
-      <NavBar />
-      <main className="container">
-        <Counters />
-      </main>
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    totalCounters: 0,
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        {/*<NavBar totalCounters={this.state.totalCounters} />*/}
+        <main className="container">
+          <Counters onChangedTotalCounter={this.handleTotalCounters} />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- counter-app/src/components/counters.jsx
+++ counter-app/src/components/counters.jsx
@@ -20,8 +20,6 @@ class Counters extends Component {
   };
 
   handleIncrement = (counter) => {
-    console.log(counter);
-
     const counters = [...this.state.counters];
     const index = counters.indexOf(counter);
     counters[index] = { ...counter };

8c4d942813612ef05facd9d2f152c51058932596
Adawat <adawat.c@kbtg.tech>
2020-04-24 01:59:11 +0700

    Has a simple grid system

--- gitlab-login/src/components/Description.jsx
+++ gitlab-login/src/components/Description.jsx
@@ -4,7 +4,7 @@ class Description extends Component {
   state = {};
   render() {
     return (
-      <div className="description">
+      <div className="description col-7">
         <h1>Open source software to collaborate on code</h1>
         <p>
           Manage Git repositories with fine-grained access controls that keep
--- gitlab-login/src/components/LoginBox.jsx
+++ gitlab-login/src/components/LoginBox.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class LoginBox extends Component {
   state = {};
   render() {
-    return <div className="login-box"></div>;
+    return <div className="login-box col-4"></div>;
   }
 }
 

ac421f9e77cfede6331eb24a001d88c35ba5ae0b
Adawat <adawat.c@kbtg.tech>
2020-04-24 01:25:32 +0700

    Has scss, header, and footer

---
+++ gitlab-login/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "../styles/styles.css";
+
+class Footer extends Component {
+  state = {};
+  render() {
+    return <div className="footer">This is the footer</div>;
+  }
+}
+
+export default Footer;
---
+++ gitlab-login/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "../styles/styles.css";
+
+class Header extends Component {
+  state = {};
+  render() {
+    return <div className="header">This is the header</div>;
+  }
+}
+
+export default Header;

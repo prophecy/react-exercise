c7adc5dfcf66276a69ddd50dabb272168f8b96e6
Adawat <adawat.c@kbtg.tech>
2020-04-23 14:53:22 +0700

    Revert "Now, render UI from conditional statements"
    This reverts commit e593814d5c228e57352f1952606c0732e888e560.

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -6,24 +6,31 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"],
   };
 
-  renderTags() {
-    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+  render() {
+    let classes = this.getBadgeClasses();
+
     return (
-      <ul>
-        {this.state.tags.map((tag) => (
-          <li key={tag}> {tag} </li>
-        ))}
-      </ul>
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </React.Fragment>
     );
   }
 
-  render() {
-    return (
-      <React.Fragment>
-        {this.state.tags.length === 0 && "Please create a new tag!"}
-        {this.renderTags()}
-      </React.Fragment>
-    );
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 

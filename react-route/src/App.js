f665415fb6b6c3a092b7cfdb07238d3d94572a9e
Adawat <adawat.c@kbtg.tech>
2020-04-29 15:54:20 +0700

    Has redirecting

--- react-route/src/App.js
+++ react-route/src/App.js
@@ -5,8 +5,9 @@ import Posts from "./components/posts";
 import Home from "./components/home";
 import Dashboard from "./components/admin/dashboard";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
 
 class App extends Component {
   render() {
@@ -22,7 +23,11 @@ class App extends Component {
             />
             <Route path="/posts/:year?/:month?" component={Posts} />
             <Route path="/admin" component={Dashboard} />
-            <Route path="/" component={Home} />
+
+            <Redirect from="/messages" to="/posts" />
+            <Route path="/not-found" component={NotFound} />
+            <Route path="/" exact component={Home} />
+            <Redirect to="/not-found" />
           </Switch>
         </div>
       </div>

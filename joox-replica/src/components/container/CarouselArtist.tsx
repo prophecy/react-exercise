b54211ed43ed4f0fb021bc7d39492afb299c0e76
Adawat <adawat.c@kbtg.tech>
2020-05-04 00:16:54 +0700

    Has very simple carousel

--- joox-replica/src/components/container/CarouselArtist.tsx
+++ joox-replica/src/components/container/CarouselArtist.tsx
@@ -1,9 +1,32 @@
 import * as React from "react";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 export interface CarouselAristProps {}
 
 const CarouselArist: React.SFC<CarouselAristProps> = () => {
-  return <div className="carousel carousel-artist"></div>;
+  return (
+    <div className="carousel carousel-artist">
+      {" "}
+      <CarouselProvider
+        naturalSlideWidth={100}
+        naturalSlideHeight={125}
+        totalSlides={3}
+      >
+        <Slider>
+          <Slide index={0}>I am the first Slide.</Slide>
+          <Slide index={1}>I am the second Slide.</Slide>
+          <Slide index={2}>I am the third Slide.</Slide>
+        </Slider>
+      </CarouselProvider>
+    </div>
+  );
 };
 
 export default CarouselArist;

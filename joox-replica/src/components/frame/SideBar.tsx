23565df5c5b31ffbb3e0ffaa19d4c974a08273cd
Adawat <adawat.c@kbtg.tech>
2020-05-07 03:51:14 +0700

    Has side bar

--- joox-replica/src/components/frame/SideBar.tsx
+++ joox-replica/src/components/frame/SideBar.tsx
@@ -3,7 +3,24 @@ import React from "react";
 export interface SideBarProps {}
 
 const SideBar: React.SFC<SideBarProps> = () => {
-  return <div className="side-bar"></div>;
+  return (
+    <div className="side-bar">
+      <div className="section-one">
+        <div className="choice">
+          <i className="fa fa-globe"></i>
+        </div>
+        <i className="fa fa-user"></i>
+        <i className="fa fa-diamond"></i>
+      </div>
+      <div className="section-two">
+        <i className="fa fa-user-circle"></i>
+        <i className="fa fa-music"></i>
+        <i className="fa fa-headphones"></i>
+        <i className="fa fa-random"></i>
+        <i className="fa fa-share"></i>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;

0c2a6b529fc03dfb0df3e377e0c5d0d2d274001e
Adawat <adawat.c@kbtg.tech>
2020-05-07 10:21:58 +0700

    Sidebar menu has titles

--- joox-replica/src/components/frame/SideBar.tsx
+++ joox-replica/src/components/frame/SideBar.tsx
@@ -13,29 +13,37 @@ class SideBar extends React.Component<SidebBrProps, SideBarState> {
         <div className="section-one">
           <div className="choice">
             <i className="fa fa-globe"></i>
+            {this.props.isExpanded ? <p>DISCOVER</p> : ""}
           </div>
           <div className="choice">
             <i className="fa fa-user"></i>
+            {this.props.isExpanded ? <p>เพลงของฉัน</p> : ""}
           </div>
           <div className="choice">
             <i className="fa fa-diamond"></i>
+            {this.props.isExpanded ? <p>VIP</p> : ""}
           </div>
         </div>
         <div className="section-two">
           <div className="choice">
             <i className="fa fa-user-circle"></i>
+            {this.props.isExpanded ? <p>ศิลปิน</p> : ""}
           </div>
           <div className="choice">
             <i className="fa fa-music"></i>
+            {this.props.isExpanded ? <p>อัลบัม</p> : ""}
           </div>
           <div className="choice">
             <i className="fa fa-headphones"></i>
+            {this.props.isExpanded ? <p>เพลย์ลิสต์</p> : ""}
           </div>
           <div className="choice">
             <i className="fa fa-random"></i>
+            {this.props.isExpanded ? <p>ชาร์ตเพลง</p> : ""}
           </div>
           <div className="choice">
             <i className="fa fa-share"></i>
+            {this.props.isExpanded ? <p>เรดิโอ</p> : ""}
           </div>
         </div>
       </React.Fragment>
